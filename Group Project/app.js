d5f3e66f853baa4b3ec99b53725161a7eb0005c2
kjkid <kjkid@pm.me>
2022-11-21 11:41:51 -0500

    online ordering quantities now increment/decrement

--- Group Project/app.js
+++ Group Project/app.js
@@ -1,33 +1,30 @@
 // Script that returns a welcome message based on the time of day
 
 if (new Date().getHours() <= 12) {
-	document.getElementById("welcome").innerHTML = "Good Morning!";
+   document.getElementById("welcome").innerHTML = "Good Morning!";
 
-} else if (new Date().getHours()<= 17) {
-	document.getElementById("welcome").innerHTML = "Good Afternoon!";
-	
-} else{
-	document.getElementById("welcome").innerHTML = "Good Evening!";
+} else if (new Date().getHours() <= 17) {
+   document.getElementById("welcome").innerHTML = "Good Afternoon!";
+
+} else {
+   document.getElementById("welcome").innerHTML = "Good Evening!";
 
 }
 
 
 // function that adds up checked items and prints a total to the user
-function Reciept()
-{
+function Reciept() {
    var input = document.getElementsByName("food");
    var total = 0;
-   
-   for (var i = 0; i < input.length; i++)
-   {
-      if (input[i].checked)
-      {
-         total += parseFloat(input[i].value);			 
+
+   for (var i = 0; i < input.length; i++) {
+      if (input[i].checked) {
+         total += parseFloat(input[i].value);
       }
-	 
-	 var tax = (total * .0975) + total;
+
+      var tax = (total * .0975) + total;
    }
-   document.getElementById("total").value = "$" + tax.toFixed(2); 
+   document.getElementById("total").value = "$" + tax.toFixed(2);
 }
 
 // Shopping Cart functionality
@@ -35,3 +32,38 @@ let cartContents = [];
 const cartItemsElement = document.querySelector("#cartItems");
 const cartSum = document.querySelector("#cartSum");
 const menuItem = document.querySelectorAll(".menuItem")
+
+
+// Page 3 - Online Ordering
+
+const qtyChange = document.getElementsByClassName("Qty");
+
+qtyChange.addEventListener("click", function () {
+
+});
+
+// Item quantity decrement
+function QtyDecrement() {
+   var itemQty = $('.menuButtons').find('.Qty').val();
+   if (itemQty <= 0) {
+      $('.menuButtons').find('.Qty').val(0);
+   }
+   else {
+      itemQty--;
+      $('.menuButtons').find('.Qty').val(itemQty);
+   }
+}
+
+// Item quantity increment
+function QtyIncrement() {
+   var itemQty = $('.menuButtons').find('.Qty').val();
+   if (itemQty >= 99) {
+      $('.menuButtons').find('.Qty').val(99);
+   }
+   else {
+      itemQty++
+      $('.menuButtons').find('.Qty').val(itemQty);
+   }
+}
+
+
